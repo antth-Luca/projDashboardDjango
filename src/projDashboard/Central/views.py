72d59380f0c5e44f82cbcd78dac7c5e036e72afe
Luca Anthony <lucamafort05@gmail.com>
2024-03-30 20:00:59 -0300

    Conserto de bugs

--- src/projDashboard/Central/views.py
+++ src/projDashboard/Central/views.py
@@ -49,34 +49,34 @@ class DashboardView(LoginRequiredMixin, TemplateView):
             labels.append(meses[cont])
         context['faturamento'] = {'data': json.dumps(data), 'labels': json.dumps(labels)}
 
-        # -> Top 5 vendedores
+        # -> Top 5 vendedores  #TODO: Verificar! Não está retornando os 3 resultados que deveria
         dict_tops = {}
         month_start = timezone.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
         month_end = month_start.replace(month=month_start.month % 12 + 1)
         list_tops = Venda.objects.filter(data__range=(month_start, month_end)).values('vendedor__nome').annotate(total_vendas=Sum('total')).order_by('-total_vendas')[:5]
         cont = 1
         for top in list_tops:
-            dict_tops['cont'] = top
+            dict_tops[cont] = top
             cont += 1
         context['top_vendedores'] = dict_tops
 
-        # -> Top 5 produtos
+        # -> Top 5 produtos  #TODO: Verificar! Não está retornando os 4 resultados que deveria
         dict_tops = {}
         month_start = timezone.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
         month_end = month_start.replace(month=month_start.month % 12 + 1)
         list_tops = Venda.objects.filter(data__range=(month_start, month_end)).values('produto__nome').annotate(total_vendas=Count('produto')).order_by('-total_vendas')[:5]
         cont = 1
         for top in list_tops:
-            dict_tops['cont'] = top
+            dict_tops[cont] = top
             cont += 1
         context['top_produtos'] = dict_tops
 
-        # -> (%) de clientes que voltam comprar em 3 meses  #TODO: Adicionar restrição de "comprou 2 ou mais vezes dentro de 3 meses"
+        # -> (%) de clientes que voltam comprar em 3 meses  #TODO: Adicionar restrição: "total_clientes" e "clientes_recorrentes" devem buscar clientes que compraram até 3 meses atrás
         total_clientes = Cliente.objects.count()
         clientes_recorrentes = Cliente.objects.annotate(total_compras=Count('venda')).filter(total_compras__gte=2).count()
         context['porc_clientes_voltam'] = (clientes_recorrentes / total_clientes) * 100 if total_clientes > 0 else 0
 
-        # -> (%) de clientes que se cadastraram e compraram no mesmo mês num período de três meses atrás  #TODO: Trocar "mês atual" por "mês do cadastro do cliente" na hora de recuperar as compras dos clientes
+        # -> (%) de clientes que se cadastraram e compraram no mesmo mês num período de três meses atrás  #TODO: Adicionar um range de hoje até 3 meses atrás
         three_months_ago = timezone.now() - timedelta(days=90)
         total_novos_clientes = Cliente.objects.filter(cadastro_em__gte=three_months_ago).count()
         novos_clientes_compras = Cliente.objects.filter(cadastro_em__gte=three_months_ago, venda__data__month=timezone.now().month).count()
@@ -85,7 +85,7 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         # -> Clientes cadastrados no mês atual
         context['clientes_cadastrados'] = Cliente.objects.filter(cadastro_em__month=timezone.now().month).count()
 
-        # -> Clientes atendidos no mês atual
+        # -> Clientes atendidos no mês atual  #TODO: Não contar duas vezes o mesmo cliente
         context['clientes_atendidos'] = Cliente.objects.filter(cadastro_em__month=timezone.now().month, venda__isnull=False).count()
 
         # -> Clientes sem atendimento no mês atual
