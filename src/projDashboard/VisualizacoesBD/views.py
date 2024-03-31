fe0fdcd18926fdf5241b98ac5a2f59767e3b9674
Luca Anthony <lucamafort05@gmail.com>
2024-03-31 17:18:42 -0300

    ...

--- src/projDashboard/VisualizacoesBD/views.py
+++ src/projDashboard/VisualizacoesBD/views.py
@@ -5,23 +5,40 @@ from Central.models import Cliente, Produto, Venda, Despesa
 @login_required
 def visual_clientes_view(request):
     clientes = Cliente.objects.all()
-
-    return render(request, 'VisualizacoesBD/visual-clientes.html', {'clientes': clientes})
+    dict_clientes = {}
+    cont = 0
+    for cliente in clientes:
+        dict_clientes[cont] = cliente
+        cont += 1
+    print(dict_clientes)
+    return render(request, 'VisualizacoesBD/visual-clientes.html', {'clientes': dict_clientes})
 
 @login_required
 def visual_produtos_view(request):
     produtos = Produto.objects.all()
-
-    return render(request, 'VisualizacoesBD/visual-produtos.html', {'produtos': produtos})
+    dict_produtos = {}
+    cont = 0
+    for produto in produtos:
+        dict_produtos[cont] = produto
+        cont += 1
+    return render(request, 'VisualizacoesBD/visual-produtos.html', {'produtos': dict_produtos})
 
 @login_required
 def visual_vendas_view(request):
     vendas = Venda.objects.all()
-
-    return render(request, 'VisualizacoesBD/visual-vendas.html', {'vendas': vendas})
+    dict_vendas = {}
+    cont = 0
+    for venda in vendas:
+        dict_vendas[cont] = venda
+        cont += 1
+    return render(request, 'VisualizacoesBD/visual-vendas.html', {'vendas': dict_vendas})
 
 @login_required
 def visual_despesas_view(request):
     despesas = Despesa.objects.all()
-
+    dict_despesas = {}
+    cont = 0
+    for despesa in despesas:
+        dict_despesas[cont] = despesa
+        cont += 1
     return render(request, 'VisualizacoesBD/visual-despesas.html', {'despesas': despesas})
